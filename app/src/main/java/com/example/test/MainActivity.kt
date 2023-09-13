8245aa45d5ff36f1be464f32cffe871a70dd2acb
miteshsingla <miteshhsingla@gmail.com>
2023-09-13 22:59:08 +0530

    Bottom Sheet and maps implemented

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -1,19 +1,137 @@
 package com.example.test
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.R
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
+import android.util.Log
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.test.databinding.ActivityMainBinding
-import com.google.firebase.auth.FirebaseAuth
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var auth:FirebaseAuth
-    private lateinit var binding:ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var googleMap: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.button.setOnClickListener{
-            auth.signOut()
+
+        // Initialize the FusedLocationProviderClient
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapView = binding.mapView
+        mapView.onCreate(savedInstanceState)
+        mapView.onResume()
+        mapView.getMapAsync(this)
+
+
+
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        this.googleMap = googleMap
+
+        // Check for location permission
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            // Permission granted, enable location features
+            googleMap.isMyLocationEnabled = true
+            getAndShowCurrentLocation()
+        } else {
+            Log.d("mitesh","else callled")
+            // Request location permission
+            ActivityCompat.requestPermissions(
+
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                LOCATION_PERMISSION_REQUEST_CODE
+            )
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.mapView.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.mapView.onLowMemory()
+    }
+
+
+    private fun getAndShowCurrentLocation() {
+        Log.d("mitesh","fun call")
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            fusedLocationClient.lastLocation.addOnSuccessListener {
+
+                    location: Location? ->
+                location?.let {
+                    Log.d("mitesh","current call")
+                    val currentLatLng = LatLng(location.latitude, location.longitude)
+                    googleMap.addMarker(MarkerOptions().position(currentLatLng).title("My Location"))
+                    googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 15f))
+                }
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            LOCATION_PERMISSION_REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getAndShowCurrentLocation()
+                } else {
+                    showToast("Location permission denied")
+                }
+            }
+        }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
     }
 }
